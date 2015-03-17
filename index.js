9fd2d5403b07978141a763a37547d2b9c4054be8
Geovani de Souza <geovanisouza92@gmail.com>
2015-03-17 09:18:37 -0300

    Adding tracking and filtering notifications

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var express = require('express'),
     morgan = require('morgan'),
     githubApi = require('./node-github'),
-    RSS = require('rss');
+    RSS = require('rss'),
+    request = require('request');
 
 var app = express(),
     github = new githubApi({
@@ -40,18 +41,22 @@ app.get('/feed', function(req, res) {
     github.notifications.getAll({
         all: true
     }, function(err, ghres) {
+        var items = ghres.filter(function(it) {
+            return !!it.unread;
+        });
         console.timeEnd('GitHub API query');
 
         console.time('Feed generation');
         var feed = new RSS(feedOptions);
-        for (var i = 0; i < Math.min(ghres.length, 20); i++) {
-            var item = ghres[i];
+        for (var i = 0; i < Math.min(items.length, 20); i++) {
+            var item = items[i];
+            var id = new Buffer(item.subject.url).toString('base64');
             feed.item({
                 title: item.subject.title,
                 description: 'From ' + item.repository.full_name + ' (' + item.repository.description + ')',
-                url: item.subject.url.replace('api.', '').replace('repos/', ''),
+                url: req.protocol + '://' + req.get('host') + '/track/' + id,
                 date: item.updated_at,
-                guid: '' + item.url + '@' + item.last_read_at,
+                guid: id,
             });
         }
         console.timeEnd('Feed generation');
@@ -60,6 +65,24 @@ app.get('/feed', function(req, res) {
         res.send(feed.xml());
         console.timeEnd('Request time');
     });
+});
+
+app.get('/track/:url', function(req, res) {
+    var url = new Buffer(req.param('url'), 'base64').toString('ascii');
+    request({
+        url: url,
+        headers: {
+            'User-Agent': 'Github-To-RSS-App/1.0',
+        },
+    }, function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+            var obj = JSON.parse(body);
+            var redir = obj._links.html.href;
+            res.redirect(redir);
+        } else {
+            res.status(500).send(response);
+        }
+    })
 });
 
 app.listen(app.get('port'), function() {
